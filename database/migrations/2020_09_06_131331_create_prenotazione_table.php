d9defca7e8f685245871c562fb40b1e5b1a6d2b7
mariateresalucchesi <mar.ter1996@gmail.com>
2020-09-06 21:27:59 +0200

    Prenotazione Tabella Model Controller Ci sono delle modifiche da apportare, come ad esempio cliente_email (messo momentaneamente)

--- database/migrations/2020_09_06_131331_create_prenotazione_table.php
+++ database/migrations/2020_09_06_131331_create_prenotazione_table.php
@@ -14,16 +14,21 @@ class CreatePrenotazioneTable extends Migration
     public function up()
     {
         Schema::create('prenotazione', function (Blueprint $table) {
-            
-           $table->string('camera');
-           $table->date('data_checkin');
-           $table->date('data_checkout');
-           $table->string('cliente');
-           $table->integer('num_persone');
-           $table->integer('importo');
-           $table->string('metodo_pagamento');
-           $table->boolean('check_pernottamento');
+            $table->id();
+            $table->string('camera_numero');
+            $table->date('data_checkin');
+            $table->date('data_checkout');
+            $table->string('cliente_email');   //modifica il nome inserendo la chiave di cliente
+            $table->integer('num_persone');
+            $table->integer('importo');
+            $table->string('metodo_pagamento');
+            $table->boolean('check_pernottamento');
 
+            $table->foreign('camera_numero')->references('numero')->on('camera')->onDelete('cascade');
+            $table->unique(['camera_numero', 'data_checkin', 'data_checkout'], 'camera_datain_dataout');
+            
+            $table->foreign('cliente_email')->references('email')->on('cliente')->onDelete('cascade');
+           //valutare se mettere cascade oppure set_null
         });
     }
 
