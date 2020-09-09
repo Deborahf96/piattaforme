0e91784875ed80849a8554e57441ee4cae6af048
mariateresalucchesi <mar.ter1996@gmail.com>
2020-09-09 23:54:49 +0200

    Modifica dall'inglese all'italiano del messaggio di errore quando si sbagliano le credenziali di accesso Aggiunta di una frase di benvenuto nella home con relativo nome utente

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.app')
 
+@php $cliente=Auth::user()->name
+@endphp
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{__('Benvenuto!')}}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +17,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __("Hai eseguito l'accesso come " .$cliente) }}
                 </div>
             </div>
         </div>
