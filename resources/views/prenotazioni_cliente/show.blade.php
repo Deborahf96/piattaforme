3100055ace68b344d5ffcef7ad7ed6d47151bc7b
Deborahf96 <debyf96@gmail.com>
2020-09-12 17:32:11 +0200

    Aggiunta elenco 'attivita selezionate' nella show di prenotazione lato cliente

--- resources/views/prenotazioni_cliente/show.blade.php
+++ resources/views/prenotazioni_cliente/show.blade.php
@@ -33,6 +33,19 @@
                     <div class="col-md-2"><b>Importo</b></div>
                     <div class="col-md-3 col-md-offset-1">{{ $prenotazione->importo }} €</div>
                 </div>
+                <hr>
+                <div class="row">
+                    <div class="col-md-2"><b>Attività selezionate</b></div>
+                    <div class="col-md-10 col-md-offset-1">
+                        <ul>
+                            @foreach ($prenotazione->attivita as $singola_attivita)
+                                <li>
+                                    {{ \Carbon\Carbon::parse($singola_attivita->data)->format("d/m/Y") }}  -  {{ \Carbon\Carbon::parse($singola_attivita->ora)->format("H:i") }}  |  {{ $singola_attivita->destinazione }}  |  {{ $singola_attivita->tipologia }}
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
